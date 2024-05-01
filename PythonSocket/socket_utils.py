189812e951fe86d0e9091ac6bbaacf56210c698b
Terry Tu Nguyen <nogias9x@gmail.com>
2024-05-01 19:56:43 +0700

    Fix socket crashed when receiving big image

--- PythonSocket/socket_utils.py
+++ PythonSocket/socket_utils.py
@@ -41,11 +41,15 @@ def receive_file(conn, filename):
 
     # convert to int, the expected file length
     expected_size = int.from_bytes(expected_size, 'big')
+    max_chunk_size_bytes = min(
+        1048576,  # 1MB
+        expected_size,
+    )
 
     # until we've received the expected amount of data, keep receiving
     packet = b""  # use bytes, not str, to accumulate
     while len(packet) < expected_size:
-        buffer = conn.recv(expected_size - len(packet))
+        buffer = conn.recv(max_chunk_size_bytes)
         if not buffer:
             raise Exception("Incomplete file received")
         packet += buffer
@@ -56,7 +60,8 @@ def receive_file(conn, filename):
 
 
 def send_file(conn, file_path, buffer_size=8):
-    conn.sendall(os.stat(file_path).st_size.to_bytes(8,'big'))
+    file_size_bytes = os.stat(file_path).st_size
+    conn.sendall(file_size_bytes.to_bytes(8,'big'))
     with open(file_path, 'rb') as file:  # open our file to read
         while True:
             chunk = file.read(buffer_size)  # get next chunk
