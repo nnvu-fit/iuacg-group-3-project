12a62cd1b85f640484ac8ca4417023b20122a19a
Terry Tu Nguyen <tu.nguyen@funnelbeam.com>
2024-05-02 17:32:28 +0700

    Fix socket crashed for some images

--- PythonSocket/server.py
+++ PythonSocket/server.py
@@ -1,4 +1,5 @@
 import socket
+from socket import SOL_SOCKET, SO_REUSEADDR
 import json
 from constants import HOST, PORT, PATH_SPLIT
 import socket_utils as su
@@ -24,7 +25,9 @@ def _format_landmark(lm):
     return result
 
 def start_server():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    with s:
         s.bind((HOST, PORT))
         s.listen()
         print(f"Listening on port {PORT}")
@@ -34,18 +37,23 @@ def start_server():
             i = 0
             while True:
                 image_path = f"ignored{PATH_SPLIT}received-image-{i}-{str(uuid4())}.png"
-                su.receive_file(conn, image_path)
+                received_file = su.receive_file(conn, image_path)
+                if not received_file:
+                    continue
 
+                response = {"success": False}
                 # detect face, if no face detected, send error message, otherwise send landmark data
                 face_detected = detect_face(image_path)
                 if not face_detected:
                     # send error message
-                    su.send_string(conn, json.dumps({"error": "No face detected"}))
+                    response["error"] = "No face detected"
                 else:
                     # send landmark data
                     detected_lm = landmark(image_path)
                     lm_json = _format_landmark(detected_lm)
-                    su.send_string(conn, json.dumps(lm_json))
+                    response["success"] = True
+                    response.update(lm_json)
+                su.send_string(conn, json.dumps(response))
                 os.remove(image_path)  # delete temp image
                 i += 1
 
--- PythonSocket/socket_utils.py
+++ PythonSocket/socket_utils.py
@@ -5,16 +5,9 @@ byte_order = 'little'
 
 def receive_string(conn):
     # get the expected length (eight bytes long, always)
-    expected_size = b""
-    while len(expected_size) < 8:
-        more_size = conn.recv(8 - len(expected_size))
-        if not more_size:
-            raise Exception("Short file length received")
-        expected_size += more_size
-
-    # convert to int, the expected file length
-    expected_size = int.from_bytes(expected_size, byte_order)
-    print(f"expected_size={expected_size}")
+    expected_size_byte_len = 4
+    expected_size_bytes = conn.recv(expected_size_byte_len)
+    expected_size = int.from_bytes(expected_size_bytes, byte_order)
 
     # until we've received the expected amount of data, keep receiving
     packet = ""
@@ -30,34 +23,36 @@ def send_string(conn: socket, string):
     conn.sendall(string.encode())  # send the string
 
 def receive_file(conn: socket, filename):
-    # receive integer of buffer length from client 
-    file_size_bytes = conn.recv(4)
-    file_size = int.from_bytes(file_size_bytes, byte_order)
-    print(f"Receiving file of size {file_size} bytes")
+    # receive integer of buffer length from client
+    expected_size_byte_len = 4
+    expected_size_bytes = conn.recv(expected_size_byte_len)
+    expected_size = int.from_bytes(expected_size_bytes, byte_order)
 
-    # receive buffer from client
-    buffer = conn.recv(file_size)
+    if not expected_size:
+        print(f"return False")
+        return False
 
-    # transform buffer to png file format
-    packet = bytearray()
-    packet.extend(buffer)
+    max_chunk_size_bytes = min(
+        1048576,  # 1MB
+        expected_size,
+    )
 
-    # join current path with filename
-    fullPath = os.path.join(os.getcwd(), filename)
-    # check if file exists
-    if os.path.exists(fullPath):
-        os.remove(fullPath)
-    # create folder if not exists
-    os.makedirs(os.path.dirname(fullPath), exist_ok=True)
-    # write the file
-    with open(fullPath, 'wb') as file:
+    # until we've received the expected amount of data, keep receiving
+    packet = b""  # use bytes, not str, to accumulate
+    while len(packet) < expected_size:
+        buffer = conn.recv(max_chunk_size_bytes)
+        if not buffer:
+            raise Exception("Incomplete file received")
+        packet += buffer
+    with open(filename, 'wb') as file:
         file.write(packet)
     print(f"Received file")
+    return True
 
 
 def send_file(conn, file_path, buffer_size=8):
     file_size_bytes = os.stat(file_path).st_size
-    conn.sendall(file_size_bytes.to_bytes(8, byte_order))
+    conn.sendall(file_size_bytes.to_bytes(4, byte_order))
     with open(file_path, 'rb') as file:  # open our file to read
         while True:
             chunk = file.read(buffer_size)  # get next chunk
